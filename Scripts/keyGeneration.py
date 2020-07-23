7f183ede34a8a56d19552ebcdcad1b346444ab2c
digu-007 <jasonryder555@gmail.com>
2020-07-23 11:51:50 +0530

    Fix infinite loop conditions

--- Scripts/keyGeneration.py
+++ Scripts/keyGeneration.py
@@ -8,15 +8,27 @@ def keyGeneration(p, q):
 
     d, cur = 0, 1
     over = 0
+    fir = 0
     while True:
         over += 1
         cur += phi
         if (cur % e == 0):
             d = cur // e
             break
-        if over > 100000000:
-            return (0, 0, 0)
-        
+
+        if over > 100000:
+            if fir > 1:
+                return (0, 0, 0)
+            else:
+                e += 2
+                while (e < phi and phi % e == 0):
+                    e += 2
+
+                d = 0
+                over = 0
+                cur = 1
+                fir += 1
+
     return (n, e, d)
 
 
